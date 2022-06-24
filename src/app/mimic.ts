39862ede516eb0ce71c43375d65de24fa5b2b85f
florian.wilk <florian.wilk@gmail.com>
2022-06-24 09:26:40 +0200

    refactorings

--- src/app/mimic.ts
+++ src/app/mimic.ts
@@ -18,18 +18,20 @@ export class Mimic extends Object3D {
     shaderMaterial: THREE.ShaderMaterial;
     uniforms: { thickness: { type: string; value: number; }; opacity: { type: string; value: number; }; diffuse: { type: string; value: THREE.Color; }; };
     anchors: Vector3[] = []
+
     constructor(anschors: Vector3[]) {
         super()
 
         const box = new THREE.SphereBufferGeometry(0.05);
         const boxmesh = new Mesh(box, new MeshBasicMaterial({ color: 0x000000 }))
-        this.add(boxmesh)
+        //this.add(boxmesh)
 
-        anschors.forEach(a=>{
+        anschors.forEach(a => {
 
-                this.anchors.push(a.clone())
-                for(var i=0;i<3;i++){
-                this.anchors.push(a.clone().add(new Vector3(Math.random()*0.2-0.1,Math.random()*0.2-0.1,0)))
+            this.anchors.push(a.clone())
+            const rr=0.1
+            for (var i = 0; i < Math.random()*5+2; i++) {
+                this.anchors.push(a.clone().add(new Vector3(Math.random() * rr - rr/2, Math.random() * rr - rr/2, 0)))
             }
 
         })
@@ -91,13 +93,9 @@ export class Mimic extends Object3D {
         this.shaderMaterial = new THREE.ShaderMaterial({
 
             uniforms: this.uniforms,
-            //            attributes: this.attributes,
             vertexShader: vertexShader,
             fragmentShader: fragmentShader,
-//            blending: THREE.AdditiveBlending,
             depthTest: true,
-  //          transparent: true
-
         });
 
         this.shaderMaterial.linewidth = 1;
@@ -108,84 +106,69 @@ export class Mimic extends Object3D {
 
         //this.geo.setDrawRange(25,10)
         //console.log(position)
+
+        const delta = this.clock.getDelta();
+        const time = this.clock.getElapsedTime() * 10;
+
         this.boxmesh.position.copy(position)
+        this.boxmesh.position.z=0.02*Math.sin(time)
         this.boxmesh.matrixWorldNeedsUpdate = true
         this.mmesh.position.copy(position)
         //        console.log(this.boxmesh.position)
         this.boxmesh.matrixAutoUpdate = true
         this.matrixWorldNeedsUpdate = true
-
-        const delta = this.clock.getDelta();
-        const time = this.clock.getElapsedTime() * 10;
         //        this.group.clear()
 
-        const mdd=0.9
-        
+        const mdd = 1.3
+
         this.anchors.forEach(a => {
             var d = a.distanceTo(this.boxmesh.position)
-            const moa=this.mo.get(a)
-            var md=mdd
-            if(moa){
-                md+=moa.dist
+            const moa = this.mo.get(a)
+            var md = 0
+            if (moa) {
+                md = moa.dist
             }
-            if (d < md) {
+            if (d < (mdd - md)) {
                 if (!this.ms.has(a)) {
-                    const curve = new THREE.CubicBezierCurve3(
-                        a,
-                        new Vector3(a.x, a.y, 0.5),
-                        new Vector3(this.boxmesh.position.x, this.boxmesh.position.y, 0.2),
-                        this.boxmesh.position,
-                    );
-                    const points2 = [];
-                    const points = [];
-                    points2.push(new Vector3(a.x,a.y+.002,a.z))
-                    //points2.push(new Vector3(a.x+.002,a.y+.02,a.z))
-                    const points3=points2.concat(curve.getPoints(20))
-                    //points.push(new THREE.Vector3(loc1.x, loc1.y, loc1.z));
-                    //points.push(new THREE.Vector3(loc2.x, loc2.y, loc2.z));//pos.x, pos.y, pos.z));
-                    const geometry = new THREE.BufferGeometry().setFromPoints(points3);
+                    const geometry = new THREE.BufferGeometry().setFromPoints([new Vector3(0, 0, 0)]);
                     this.ms.set(a, geometry)
 
                     const curveObject = new THREE.Line(geometry, this.linematerial);
-                    const arr = [3,0.2,3,2,3,4,2]
-                    geometry.setAttribute("thickness", new THREE.Float32BufferAttribute(arr,50));
+                    const arr = [3, 0.2, 3, 2, 3, 4, 2]
+                    geometry.setAttribute("thickness", new THREE.Float32BufferAttribute(arr, 50));
 
-                    geometry.setDrawRange(0,0)
+                    geometry.setDrawRange(0, 0)
                     this.group.add(curveObject)
                     this.mc.set(a, curveObject)
-                    this.mo.set(a,{ dist:Math.random()*0.6,erand: Math.random()*0.06-0.03, orand: Math.random()*0.04-0.02})
-                } else {
-
-                    const curve = new THREE.CubicBezierCurve3(
-                        a,
-                        new Vector3(a.x, a.y, 0.2),
-                        new Vector3(this.boxmesh.position.x, this.boxmesh.position.y, 0.2),
-                        this.boxmesh.position,
-                    );
-                    const points2 = [];
-                    const points = [];
-                    const erand = this.mo.get(a).erand;
-                    const orand = this.mo.get(a).erand;
-                    points2.push(new Vector3(a.x,a.y+erand,a.z))
-                    points2.push(new Vector3(a.x+erand,a.y+erand,a.z))
-                    //points2.push(new Vector3(a.x+.2,a.y+.02,a.z))
-                    const points3=points2.concat(curve.getPoints(20))
-                    const e = this.ms.get(a)
-                    points3.forEach((p,i)=>{
-                        p.z+=Math.sin(Math.PI/20*i)*orand
-                    } )
-                    if (e) {
-                        e.setFromPoints(points3)
-                    }
+                    if (!moa)
+                        this.mo.set(a, { dist: Math.random() * 0.6, erand: Math.random() * 0.06 - 0.03, orand: Math.random() * 0.04 - 0.02 })
                 }
 
+                const curve = new THREE.CubicBezierCurve3(
+                    a,
+                    new Vector3(a.x, a.y, 0.2),
+                    new Vector3(this.boxmesh.position.x, this.boxmesh.position.y, 0.2),
+                    this.boxmesh.position,
+                );
+                const points2 : Vector3[]= [];
+                const points = [];
+                const erand = this.mo.get(a).erand;
+                const orand = this.mo.get(a).erand;
+                //points2.push(new Vector3(a.x, a.y + erand, a.z))
+                //points2.push(new Vector3(a.x + erand, a.y + erand, a.z))
+                //points2.push(new Vector3(a.x+.2,a.y+.02,a.z))
+                const points3 = points2.concat(curve.getPoints(20))
                 const e = this.ms.get(a)
+                points3.forEach((p, i) => {
+                    p.z += Math.sin(Math.PI / 20 * i) * orand
+                })
                 if (e) {
-                    const minr = 0.9*md
-                    const maxr = 1*md
-                    const dd = Math.max(0, 22 / (maxr-minr) * (d-minr));
-                    
-                    e.setDrawRange(dd,22)
+                    e.setFromPoints(points3)
+                    const minr = 0.9 * (mdd - md)
+                    const maxr = 1 * (mdd - md)
+                    const dd = Math.max(0, points3.length / (maxr - minr) * (d - minr));
+
+                    e.setDrawRange(dd, points3.length)
                 }
 
             } else {
